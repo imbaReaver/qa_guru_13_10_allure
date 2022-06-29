b235619399bda176028b24d276501328974b98c3
Artem_Bobkov <abobkov@dnomads.pro>
2022-06-29 17:52:05 +0700

    Refactoring

--- src/test/java/tests/LambdaStepsTest.java
+++ src/test/java/tests/LambdaStepsTest.java
@@ -8,7 +8,6 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 
-import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.*;
 import static io.qameta.allure.Allure.step;
 
